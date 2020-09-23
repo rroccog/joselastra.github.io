61d2708f3ec755610b0b0da2396a843eda2f5587
JoseLastra <jose.lastra@pucv.cl>
2020-09-23 17:57:09 -0300

    update class

--- geo459/clase6/06_rasterApp/app.R
+++ geo459/clase6/06_rasterApp/app.R
@@ -6,10 +6,11 @@ library(raster)
 library(rasterVis)
 library(shinybusy)
 
-ui <- fluidPage( ## armemos nuestra appa simple
-  add_loading_state(".shiny-plot-output", timeout = 5000,messageFontSize = 50 ,text = "Cargando gráfico...",
-    svgColor = "steelblue"),
-  'Explorador de temperatura de Chile' %>% strong() %>% h4(),
+##nuestra interfaz de usuario
+ui <- fluidPage(
+  add_loading_state(".shiny-plot-output",text = 'Cargando gráficos...', svgColor = 'steelblue'),
+  ##titulo y cuerpo de UI
+  "Explorador de temperatura Chile" %>% strong() %>% h4(),
   uiOutput(outputId = 'regiones'),## crearemos el input en el server
   plotOutput('rasPlot'),
   plotOutput('histo',width = '1000px') %>% column(width = 4,offset = 3)
@@ -18,24 +19,23 @@ ui <- fluidPage( ## armemos nuestra appa simple
 server <- function(input, output, session) {
   ##load data
   temp.data <- stack('data/chile_temp.tif')
-  chile <- read_sf('data/Regional.shp')
+  chile <- st_read('data/Regional.shp')
   names(temp.data) <- c('min','mean','max')
-
-  #render uiOutput
-  regionInput <- chile$Region %>% unique()
-  
+  ###############
+  regionInput <- chile$Region %>% unique() #nombres de las regiones
+  #render de selección de regiones
   output$regiones <- renderUI({
-      selectInput(inputId = "region",label = "Seleccione una Región:",choices = regionInput[1:16],multiple = F )
-    })
+    selectInput(inputId = "region",label = "Seleccione una Región:",choices = regionInput[1:16],multiple = F)
+  })
   
   #reactive data
   data.select <- function (){
     req(input$region)
-    shp.crop <- chile %>% filter(Region == input$region)
-    r.plot <- temp.data %>% crop(shp.crop) %>% mask(shp.crop)
+    shp.crop <- chile %>% filter(Region == input$region) #selección por atributos
+    r.plot <- temp.data %>% crop(shp.crop) %>% mask(shp.crop) # crop and mask
     r.plot
   }
- 
+  
   #render rasPlot
   output$rasPlot <- renderPlot({
     req(data.select())
@@ -45,15 +45,27 @@ server <- function(input, output, session) {
   ##render histo
   output$histo <- renderPlot({
     req(data.select())
-    r.df <- data.select() %>% as('SpatialPixelsDataFrame') %>% as.data.frame()
-    names(r.df) <- c('min','mean','max','x','y')
-    r.dfPlot <- r.df %>% melt(id=c('x','y'))
+     r.df <- data.select() %>% 
+      as('SpatialPixelsDataFrame') %>% as.data.frame() # pasando a data frame
+    names(r.df) <- c('min','mean','max','x','y') #cambiar nombres a columnas
+    r.dfPlot <- r.df %>% melt(id=c('x','y')) # arreglar datos para ploteo de histograma
     
     ggplot(data = r.dfPlot, aes(x=value/10, fill=variable)) +
       geom_histogram(alpha=0.6, position = 'identity',bins = 20) + theme_bw()+ labs(fill="")+
       xlab('Temperatura') + ylab("Frecuencia")
   })
-  
 }
 
 shinyApp(ui, server)
+
+
+
+
+
+
+
+
+
+
+
+
