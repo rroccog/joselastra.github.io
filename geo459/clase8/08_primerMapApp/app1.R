2e0e81094676795cd1a70512ed96cc6466a6d19b
JoseLastra <jose.lastra@pucv.cl>
2020-09-27 18:28:49 -0300

    update class

--- geo459/clase8/08_primerMapApp/app1.R
+++ geo459/clase8/08_primerMapApp/app1.R
@@ -10,21 +10,23 @@ ui <- bootstrapPage(
   absolutePanel(id="controls",
                 style="z-index:500;", top = 90, left = "auto", right = 20, 
                 bottom = "auto",
-                width = 300, height ="auto",
+                width = 400, height ="auto",
                 class = "panel panel-default",
-    selectInput(inputId = 'campo',label = 'Seleccione campo a visualizar',
-                choices = c(1,2,3))
+                selectInput(inputId = 'campo',label = 'Seleccione campo a visualizar',
+                            choices = c(1,2,3)),
+                actionButton('remap',label = 'Mostrar información seleccionada')
   )
-
 )
 
 server <- function(input, output, session) {
   
   #Load data
-  shp <- st_read('data/Pymp_05_2016.shp')
+  shp <- read_sf('data/viña_del_mar.shp') %>% st_transform(4326)
   ## rendering base map
   output$map <- renderLeaflet({
-    leaflet() %>% addProviderTiles(providers$CartoDB.DarkMatter)
+    leaflet() %>% addProviderTiles(providers$CartoDB.DarkMatter) %>% 
+      addPolygons(data = shp,group = 'Manzanas') %>%
+      addLayersControl(overlayGroups = 'Manzanas',position = 'topleft')
   })
 }
 
